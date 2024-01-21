5b9c0f4877a284844b71a9a5fb7aff5ccff1d91b
bagushunter313 <bagushunter313@gmail.com>
2024-01-21 22:58:15 +0700

    Day 5

---
+++ Day 5/JS/contact.js
@@ -0,0 +1,27 @@
+function submitData() {
+  const Ipname = document.getElementById("Ipnama").value;
+  const Ipemail = document.getElementById("Ipemail").value;
+  const Ipphone = document.getElementById("Ipphone").value;
+  const Ipsubject = document.getElementById("Ipsubject").value;
+  const Ippesan = document.getElementById("Ippesan").value;
+
+  if (!Ipname) {
+    alert("Nama harus diisi");
+  } else if (!Ipemail) {
+    alert("Email harus diisi");
+  } else if (!Ipphone) {
+    alert("Phone number harus diisi");
+  } else if (!Ipsubject) {
+    alert("Subject harus diisi");
+  } else if (!Ippesan) {
+    alert("Message harus diisi");
+  } else {
+    console.log(
+      `Name : ${Ipname}\nEmail: ${Ipemail}\nPhone: ${Ipphone}\nSubject: ${Ipsubject}\nMessage: ${Ippesan}`
+    );
+
+    let a = document.createElement("a");
+    a.href = `mailto:${Ipemail}?subject=${Ipsubject}&body=${Ippesan}`;
+    a.click();
+  }
+}
